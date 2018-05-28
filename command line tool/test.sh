950bf063ec8a3d44c61f8e116cc000c57dca4ddc
hxzbg <hongxianzhi@163.com>
2018-05-28 10:44:28 +0800

    test shell script

---
+++ command line tool/test.sh
@@ -0,0 +1,2 @@
+./ABParser --do compare --old /home/user/Desktop/old --new /home/user/Desktop/new --patch /home/user/Desktop/patch/test.patch
+./ABParser --do merge --old /home/user/Desktop/old --patch /home/user/Desktop/patch/test.patch --mergeto /home/user/Desktop/merge
